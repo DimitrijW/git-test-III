b29009d109e5f4a04b1d1323f0cc66c4baef6418
DimitrijW <dimitrij.code@gmail.com>
2021-11-30 11:14:06 +0100

    change file1.cpp

--- file1.cpp
+++ file1.cpp
@@ -1,23 +1,23 @@
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
+old old old old old old old
+old old old old old old old
+old old old old old old old
+old old old old old old old
+old old old old old old old
+old old old old old old old
 
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
+old old old old old old old
+old old old old old old old
+old old old old old old old
+old old old old old old old
+old old old old old old old
+old old old old old old old
 
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
+old old old old old old old
+old old old old old old old
+old old old old old old old
+old old old old old old old
+old old old old old old old
+old old old old old old old
 
 new new new new new new new
 new new new new new new new
