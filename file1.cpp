d8fb8ce888a857193c44479699806b46546ed756
DimitrijW <dimitrij.code@gmail.com>
2022-02-16 11:26:31 +0100

    delete

--- file1.cpp
+++ file1.cpp
@@ -5,32 +5,10 @@ old old old old old old old
 old old old old old old old
 old old old old old old old
 
-
 new new new new new new new
 new new new new new new new
 new new new new new new new
 new new new new new new new
 new new new new new new new
 new new new new new new new
-
-old old old old old old old
-old old old old old old old
-old old old old old old old
-old old old old old old old
-old old old old old old old
-old old old old old old old
-
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
-new new new new new new new
-
-old old old old old old old
-old old old old old old old
-old old old old old old old
-old old old old old old old
-old old old old old old old
-old old old old old old old
 
